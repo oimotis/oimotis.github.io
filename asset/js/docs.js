41ab50aeca6f58f15e455e754bc71c782bd57514
oulvhai <oulvhai@oulvhais-MacBook-Pro.local>
2016-03-18 12:39:39 +0800

    preload img

--- asset/js/docs.js
+++ asset/js/docs.js
@@ -13,3 +13,22 @@ function switchImageByName(ele,name,index){
 	ctEle.find('p').hide().eq(index).show();
 
 }
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+}else{
+	preload([
+    'asset/img/en/1-2.jpg',
+    'asset/img/en/1-3.jpg',
+    'asset/img/en/2-2.jpg',
+    'asset/img/en/3-2.jpg',
+    'asset/img/en/3-3.jpg',
+    'asset/img/en/4-2.jpg'
+	]);
+}
